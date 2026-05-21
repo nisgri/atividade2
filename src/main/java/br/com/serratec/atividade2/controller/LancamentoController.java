a09b5ee6cacbc71e147f0790e1e30a41168e9005
Douglas <dodomathias29@gmail.com>
2026-05-21 20:46:57 -0300

    swagger adicionado

--- src/main/java/br/com/serratec/atividade2/controller/LancamentoController.java
+++ src/main/java/br/com/serratec/atividade2/controller/LancamentoController.java
@@ -18,6 +18,11 @@ import java.util.*;
 
 import br.com.serratec.atividade2.dto.LancamentoVendasResponseDTO;
 import br.com.serratec.atividade2.service.LancamentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -25,11 +30,33 @@ public class LancamentoController {
     @Autowired
     private LancamentoService service;
 
+    @Operation(summary = "Lançamento vendas", description = "A resposta retorna dados de vendas")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "201", 
+			content = {@Content(schema = @Schema(implementation = LancamentoVendas.class), mediaType = "application/json")},
+			description = "Cadastra vendas"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
+			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
+
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public LancamentoVendasResponseDTO inserirLancamento(@Valid @RequestBody LancamentoVendas lancamento) {
         return service.inserirLancamento(lancamento);
     }
+
+    @Operation(summary = "Lista todas as vendas", description = "A resposta lista os dados das vendas por dataVenda, valorVenda e nomeVendedor.")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", 
+			content = {@Content(schema = @Schema(implementation = LancamentoVendas.class), mediaType = "application/json")},
+			description = "Retorna todas as vendas"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
+			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
+
 
     @GetMapping("{id}")
     public ResponseEntity<LancamentoVendasResponseDTO> listarPorId(@PathVariable Long id) {
